b7840939e1e2726f9a0141b0d7974f52717e2a9c
Gilson Jr <gilsondiasjn15@gmail.com>
2024-03-09 12:48:15 -0300

    menu mobile com botão burger

--- JS_geral/js.js
+++ JS_geral/js.js
@@ -1,8 +1,20 @@
-const mostar = document.querySelector("#menu_icone")
-const menu = document.querySelector(".esconder")
-const fechar = document.querySelector(".ultimo_menu")
+const mostar = document.querySelector("#menu_icone");
+const menu = document.querySelector(".esconder");
+const fechar = document.querySelector(".ultimo_menu");
 
+// Animando o menu do mobile
+const menuMobile  = document.getElementById("menu-mobile");
+const burgerMenu  = document.getElementById("btn-menu");
 
+// Animando o menu mobile
+menuMobile.addEventListener('click', animarMenu);
+
+// Função pra abrir o menu no mobile e animar o burger
+function animarMenu(){
+  // Caso não exista a classe determinanda nos (''), o toggle irá adicionar
+  menuMobile.classList.toggle('abrirMenu');
+  burgerMenu.classList.toggle('active');
+}
 
 mostar.addEventListener("click", function(){
   menu.classList.add("block")
