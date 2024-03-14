88bc1909240bca6769820b37829236566c1d94e9
Gilson Jr <gilsondiasjn15@gmail.com>
2024-03-14 00:39:13 -0300

    Reorganizando código do menu e acrescentando menu nas demais pág (memória e videos)

--- JS_geral/js.js
+++ JS_geral/js.js
@@ -1,13 +1,13 @@
 // Animando o menu do mobile
-const menuMobile  = document.getElementById("menu-mobile");
+const menu        = document.getElementById("menu");
 const burgerMenu  = document.getElementById("btn-menu");
 
 // Animando o menu mobile
-menuMobile.addEventListener('click', animarMenu);
+menu.addEventListener('click', animarMenu);
 
 // Função pra abrir o menu no mobile e animar o burger
 function animarMenu(){
   // Caso não exista a classe determinanda nos (''), o toggle irá adicionar
-  menuMobile.classList.toggle('abrirMenu');
+  menu.classList.toggle('abrirMenu');
   burgerMenu.classList.toggle('active');
 }
