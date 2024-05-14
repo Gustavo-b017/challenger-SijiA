8c705b89cff11a1dd724baf8c4e0034e115a0ee1
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-13 23:13:16 -0300

    atualização da pg home

--- src/Paginas/Home/Componentes/Caracteristicas.jsx
+++ src/Paginas/Home/Componentes/Caracteristicas.jsx
@@ -2,27 +2,42 @@ import './css/caracteristicas.css'
 import nuvem1 from '../Img/nuvem1.png'
 import nuvem2 from '../Img/nuvem2.png'
 import nuvem3 from '../Img/nuvem3.png'
+import i_jogo from '../Img/icon_jogo.svg'
+import i_quiz from '../Img/icon_quiz.svg'
+import i_video from '../Img/icon_video.svg'
 
 export default function Caracteristicas() {
   return (
     <div className="container-nuvens">
+        
         <div className="nuvem">
-            <img src={nuvem1} class="img-fluid nuvens-img" alt="videos"></img>
-            <p className="nuvem-text">
-                Vídeos curtos  sobre exames e diagnósticos.
-            </p>
+            <img src={nuvem1} class="img-fluid nuvens-img" alt="nuvem"></img>
+            <div className="content">
+                <img src={i_video} class="img-fluid" alt="videos"></img>
+                <span className="nuvem-text">
+                    Vídeos curtos  sobre exames e diagnósticos.
+                </span>
+            </div>
         </div>
+
         <div className="nuvem">
             <img src={nuvem2} class="img-fluid nuvens-img" alt="jogos"/>
-            <p className="nuvem-text">
-                Jogos dinâmicos para entreter e informar.
-            </p>
+            <div className="content">
+                <img src={i_jogo} class="img-fluid" alt="jogos"></img>
+                <span className="nuvem-text">
+                    Jogos dinâmicos para entreter e informar.
+                </span>
+            </div>
         </div>
+
         <div className="nuvem">
             <img src={nuvem3} class="img-fluid nuvens-img" alt="conhecimento"/>
-            <p className="nuvem-text">
-                Feedbacks e tratamento personalizado. 
-            </p>
+            <div className="content">
+                <img src={i_quiz} class="img-fluid" alt="quiz"></img>
+                <span className="nuvem-text">
+                    Feedbacks e tratamento personalizado. 
+                </span>
+            </div>
         </div>
     </div>
   )
