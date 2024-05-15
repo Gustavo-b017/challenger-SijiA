e7482fdaac9d18cdcf74a7de5d9ec8d3dc7ddff8
Gustavo <gustavo.estudo017@gmailcom>
2024-05-15 18:08:47 -0300

    finalizando mudanca de styled para css normal da pagina memoria

--- src/Paginas/Memoria/Memoria.jsx
+++ src/Paginas/Memoria/Memoria.jsx
@@ -1,24 +1,12 @@
-import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
-import Inicio from './Componentes/MemoriaHemodialize';
+import './Componentes/memoria.css';
 
-const Conteudo = styled.main `
-  padding: 2% 5%;
-  display: flex;
-  flex-direction: column; 
-  justify-content: center;
-  
-  @media screen and (max-width: 800px) {
-    padding: 1% 2%;
-    margin: 0 auto;
-  }
-`
 
 function Memoria() {
   return (
-    <Conteudo>
+    <main className='mainMemoria'>
       <Outlet/>
-    </Conteudo>
+    </main>
   );
 }
 
