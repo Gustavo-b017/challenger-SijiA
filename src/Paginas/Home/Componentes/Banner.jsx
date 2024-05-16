51d76cdbcb89c10067eb7aec44bb1fdcfb1a8d1f
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-16 11:08:20 -0300

    .

--- src/Paginas/Home/Componentes/Banner.jsx
+++ src/Paginas/Home/Componentes/Banner.jsx
@@ -2,7 +2,7 @@ import FotoBanner from '../Img/banner.png';
 
 export default function Banner() {
   return (
-    <div className="banner-box">
+    <div className="banner-home">
         <img src={FotoBanner} class="img-fluid" alt="banner"></img>
     </div>
   )
