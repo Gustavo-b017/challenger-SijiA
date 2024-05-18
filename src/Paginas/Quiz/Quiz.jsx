c456ade65be652c1f09236cabc225f844e916619
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-18 11:53:14 -0300

    .

--- src/Paginas/Quiz/Quiz.jsx
+++ src/Paginas/Quiz/Quiz.jsx
@@ -7,7 +7,10 @@ export default function Quiz() {
   return (
     <main>
       <div className="main">
-        <section className='home'>
+        <section className='home-quiz'>
+          <div className="banner-quiz">
+            {/* <img src={banner} alt="banner" className="banner" /> */}
+          </div>
           <div className="home-content">
             <h1>Pagina do Quiz</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci dolore iure non rerum blanditiis nemo corrupti dolor dignissimos ex laboriosam repellendus delectus</p>
