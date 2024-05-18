7480dcb7bc083f08aa3366db4151d4b88af31102
Felipe Ferreira <felipeferreira.tech@outlook.com>
2024-05-18 20:12:43 -0300

    Atualizando responsividade da home

--- src/Paginas/Home/Home.jsx
+++ src/Paginas/Home/Home.jsx
@@ -2,11 +2,12 @@ import Banner from "./Componentes/Banner";
 import Caracteristicas from "./Componentes/Caracteristicas";
 import Grupo from "./Componentes/Grupo";
 import SobreProjeto from "./Componentes/SobreProjeto"
+import "./Home.css"
 
 
 export default function Home() {
   return (
-      <div>
+      <div className="body">
         <Banner/>
         <Caracteristicas/>
         <SobreProjeto/>
