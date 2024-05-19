c89d1b6a2318bd9f73e3b8e4a2a0d6f89be5a5c6
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-19 01:51:37 -0300

    PopUp inicial do quiz

--- src/Paginas/Quiz/componentesQuiz/IniciapPopUp/PopUp/PopupInfo.jsx
+++ src/Paginas/Quiz/componentesQuiz/IniciapPopUp/PopUp/PopupInfo.jsx
@@ -1,24 +1,24 @@
 
-import './PopUp.css';
+import '../../../css/PopUpInicio/popUp.css';
 import { Link } from 'react-router-dom';
 import { ContinuarBtn, ExitBtn } from '../LogicaQuiz';
+import moeda from '../../../img/moeda.gif'
 
 export default function PopUpInfo() {
   return (
     <div className="popupInfo">
       <h2>Informações sobre o Quiz</h2>
-      <span className='info'> 1. Esse quiz foi desenvolvido para testar seus conhecimentos sobre os exames.</span>
-      <span className='info'> 2. Para jogar, clique no botão "Iniciar Quiz".</span>
-      <span className='info'> 3. As perguntas são de múltipla escolha, com apenas uma alternativa correta.</span>
-      <span className='info'> 4. Boa sorte!</span>
-      <span className='info'> 5. Confio em Você!</span>
+      <span className='info'><img src={moeda} alt="moeda"/> Jogo desenvolvido para testar seus conhecimentos sobre os exames.</span>
+      <span className='info'><img src={moeda} alt="moeda"/> As perguntas são de múltipla escolha, com apenas uma alternativa correta.</span>
+      <span className='info'><img src={moeda} alt="moeda"/> Para jogar, clique no botão "Iniciar Quiz".</span><br/>
+      <span className='info'><img src={moeda} alt="moeda"/> Boa sorte! Confiamos em Você!</span>
 
       <hr/>
 
       <div className="btn-group">
         <button onClick={ExitBtn} className="info-btn">Sair do quiz</button>
         <button onClick={ContinuarBtn} className="info-btn">
-          <Link to="QuizPerguntas">Iniciar Quiz</Link>
+          <Link to="QuizPerguntas" className='iniciaQuiz'>Iniciar Quiz</Link>
         
         </button>
       </div>
