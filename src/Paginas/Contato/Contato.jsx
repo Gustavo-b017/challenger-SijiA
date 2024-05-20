3e5c891fc75e8bdb46d87162883522d5718a66fd
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-20 00:33:24 -0300

    pg contato finalizada

--- src/Paginas/Contato/Contato.jsx
+++ src/Paginas/Contato/Contato.jsx
@@ -1,27 +1,54 @@
 import './contato.css'
 import img_contato from './img/nuvem-contato.png'
 import icon_nome from './img/icon-user.svg'
+import icon_email from './img/icon-email.svg'
+import icon_mensagem from './img/icon-mensagem.svg'
 
 function Contato() {
     return ( 
         <main id="contato">
-            <div className="formularioContato">
-                <h1>CONTATO</h1>
-                <div className="inputContato form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingInputGroup1" placeholder="Username"/>
-                    <label for="floatingInputGroup1"><img src={icon_nome} alt="nome" /> Username</label>
+            <div className="containerContato">
+                <div className="formularioContato">
+
+                    {/* TÍTULO */}
+                    <h1>CONTATO</h1>
+
+                    {/* CAMPO NOME */}
+                    <div class="inputContato input-group mb-3">
+                        <span class="input-group-text"><img src={icon_nome} className='imgIconsContato ' alt="nome"/></span>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Digite seu nome"/>
+                            <label for="floatingInputGroup1">Digite seu nome</label>
+                        </div>
+                    </div>
+
+                    {/* CAMPO EMAIL */}
+                    <div class="inputContato input-group mb-3">
+                        <span class="input-group-text"><img src={icon_email} className='imgIconsContato ' alt="email"/></span>
+                        <div class="form-floating">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                            <label for="floatingInput">Digite seu email</label>
+                        </div>
+                    </div>
+
+                    {/* CAMPO MENSAGEM */}
+                    <div class="inputContato input-group mb-4">
+                        <span class="input-group-text"><img src={icon_mensagem} className='imgIconsContato ' alt="nome"/></span>
+                        <div class="form-floating">
+                            <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                            <label for="floatingTextarea">Esvreva sua mensagem</label>
+                        </div>
+                    </div>
+        
+                    <button type="button" className="btnContato">ENVIAR</button>
+
                 </div>
-                <div className="inputContato form-floating mb-3">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label for="floatingInput">Email address</label>
+
+                {/* BANNER CONTATO */}
+                <div className="imgContato">
+                    <img src={img_contato}  className="img-fluid" alt="Contato"/>
                 </div>
-                <div className="inputContato form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                    <label for="floatingTextarea">Comments</label>
-                </div>
-            </div>
-            <div className="imgContato">
-                <img src={img_contato} alt="Contato" />
+
             </div>
         </main>
      );
