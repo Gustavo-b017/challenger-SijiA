d2cf32aeeef37ff29260cebb0708a9271eede287
Gustavo <gustavo.estudo017@gmailcom>
2024-05-20 16:24:08 -0300

    vai

---
+++ src/Paginas/Videos/ComponentesVideo/VideoGallery.jsx
@@ -0,0 +1,9 @@
+function VideoGallery() {
+    return ( 
+        <div className="video-gallery">
+            <h1 style={{textAlign: 'center'}}> video gallery</h1>
+        </div>
+     );
+}
+
+export default VideoGallery;
--- src/Paginas/Videos/Videos.jsx
+++ src/Paginas/Videos/Videos.jsx
@@ -1,9 +1,19 @@
-import { BtnVideo } from "./ComponentesVideo/BtnVideo";
+import VideoGallery from "./ComponentesVideo/VideoGallery";
+
+import
 
 function Videos() {
+
+    let data = [
+        {
+            id: 1,
+            poster: img1,
+        }
+    ]
     return ( 
         <div>
             <h1>videos</h1>
+            <VideoGallery/>
         </div>
     );
 }
