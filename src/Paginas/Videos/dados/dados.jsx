b191b08ad73a0621ab0e1cd3a5f3f17572c9d486
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-21 20:20:59 -0300

    ....

--- src/Paginas/Videos/dados/dados.jsx
+++ src/Paginas/Videos/dados/dados.jsx
@@ -4,13 +4,13 @@ import raiox from '../videos&img/tb_raiox.png'
 
 import hemogramaVideo from '../videos&img/video-hemograma.mp4'
 import eletrocardVideo from '../videos&img/blizzardVideo.mp4'
-import raioxVideo from '../videos&img/video-raiox.mp4'
+// import raioxVideo from '../videos&img/video-raiox.mp4'
 
 export const dados = [
     {type: "video", url: hemogramaVideo, poster: hemograma},
     
     {type: "video", url: eletrocardVideo, poster:  eletrocard},
     
-    {type: "video", url: raioxVideo, poster: raiox}
+    {type: "video", url: eletrocardVideo, poster: raiox}
     
 ]
