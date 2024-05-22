1e970250bdab98145a9ad464cd89d411ed7e2a28
Gustavo <gustavo.estudo017@gmailcom>
2024-05-21 21:28:07 -0300

    dados novos

--- src/Paginas/Quiz/data/quiz-data.js
+++ src/Paginas/Quiz/data/quiz-data.js
@@ -8,18 +8,18 @@ export let questoesGeral =  [
       "Uma varinha mágica", 
       "Uma câmera", 
       "Uma Máquina de Ressonância Magnética", 
-      "Um Skate"
+      "Um Skate",
     ],
     correct: 2,
   },
   {
     id: 2,
-    question: "Sr. Ressonância nos ajuda a descobrir problemas em nossos corpos. Que tipo de problemas ele pode encontrar ?",
+    question: "Sr. Ressonância Magnetica nos ajuda a descobrir problemas em nossos corpos. Que tipo de problemas ele pode encontrar ?",
     answers: [
       "Onde estão escondidos os tesouros", 
       "Como consertar brinquedos quebrados", 
       "Coisas estranhas que possam estar acontecendo dentro do nosso corpo", 
-      "Doces"
+      "Menino Gilsonn!!! Onde você está?",
     ],
     correct: 2,
   },
@@ -30,42 +30,118 @@ export let questoesGeral =  [
       "Nossos pés", 
       "Nossas mãos", 
       "Nossa cabeça, onde mora nosso cérebro!", 
-      "Nossos bracinhos"
+      "Nossos bracinhos",
     ],
     correct: 2,
-    
   },
   {
     id: 4,
-    question: "Dentro da máquina mágica, como devemos nos comportar?",
+    question: "Dentro da máquina mágica de tomografia, como devemos nos comportar?",
     answers: [
       "Ficando mais quietinho possível", 
       "Pulando e gritando", 
-      " Gritando bem alto", 
-      "Escondido"
+      "Gritando bem alto", 
+      "Escondido",
     ],
     correct: 0,
   },
   {
     id: 5,
-    question: "Quando você está dentro da máquina mágica, que som você ouve?",
+    question: "Quando você está dentro da máquina mágica do raio-x, que som você ouve?",
     answers: [
       "O som de um pássaro cantando", 
       "Um barulhinho engraçado, como um martelinho tocando", 
-      "Silêncio total, como se estivesse em uma caverna misteriosa", 
-      "Som de chuva"
+      "Fica frio Ai!!!", 
+      "Som de chuva",
     ],
     correct: 1,
   },
   {
     id: 6,
-    question: " Which of the following is TRUE about reflows and repaints?",
+    question:"O que os médicos podem fazer com as imagens obtidas através da Tomografia ?",
     answers: [
-      " They're the same thing.",
-      " Repaints (or redraws) occur when elements change their visual styles but not layout.",
-      " Reflows compute layout, are more performance critical, and occur when elements are inserted, removed, moved, animated, etc.",
-      " The previous two answers are correct.",
+      "Criar histórias em quadrinhos",
+      "Verificar se voçê têm superpoderes",
+      "Analisar seu corpinho para te curar",
+      "Ver os cabelos pintados",
     ],
-    correct: 3
-  }
+    correct: 2,
+  },
+  {
+    id: 7,
+    question: "Qual é o exame que utiliza ondas especias para ver o nosso corpinho?",
+    answers: [
+      "Hemodiálise", 
+      "Ecocardiograma", 
+      "Escavadeira", 
+      "Batedeira de bolo",
+    ],
+    correct: 1,
+  },
+  {
+    id: 8,
+    question: "Quem realiza o exame de eletrocardiograma?",
+    answers: [
+      "Balconista", 
+      "Super-médico de corações", 
+      "Enfermeira", 
+      "Professor",
+    ],
+    correct: 1,
+  },
+  {
+    id: 9,
+    question: "Pra que eletrocardiograma exame serve ?",
+    answers: [
+      "Para medir a febre", 
+      "Para ver a barriguinha", 
+      "Para ver os olhos", 
+      "Para descobrir se o nosso coração está funcionando direitinho",
+    ],
+    correct: 3,
+  },
+  {
+    id: 10,
+    question: "Imagine que você é um detetive e precisa de uma máquina especial para tirar fotos dentro do corpo humano. Que máquina é essa?",
+    answers: [
+      "Máquina de fazer pipoca",
+      "Máquina de fazer algodão doces",
+      "Livre Estou, Livre Estou (Elsa, Frozen)",
+      "Tomografia ",
+    ],
+    correct: 3,
+  },
+  {
+    id: 11,
+    question: "Que som você ouve quando está dentro da máquina de Tomografia?",
+    answers: [
+      "Um zumbido suave enquanto os raios-X tiram fotos",
+      "Onde você está, BB?",
+      "O som de um rio correndo",
+      "O som de chuva",
+    ],
+    correct: 0,
+  },
+  {
+    id: 12,
+    question: "O que a Tomografia Computadorizada pode detectar que outros exames não conseguem?",
+    answers: [
+      "Receitas secretas de bolo",
+      "Problemas dentro do nosso corpinho",
+      "Livros de magia",
+      "Carros de corrida",
+    ],
+    correct: 1,
+  },
+  {
+    id: 13,
+    question: "Quando você está dentro da máquina de Tomografia, como deve se comportar?",
+    answers: [
+      "Cantando alto para animar o ambiente",
+      "Ficando bem quietinho, como um espião",
+      "Fazendo piadas engraçadas",
+      "Correr pela sala",
+    ],
+    correct: 1,
+  },
 ];
