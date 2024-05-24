a50b8da8e32a29b153a89a08386b7f1a1081126a
Gustavo <gustavo.estudo017@gmailcom>
2024-05-24 14:22:59 -0300

    api feito

--- src/ComponetesGerais/Footer/Footer.jsx
+++ src/ComponetesGerais/Footer/Footer.jsx
@@ -1,76 +1,56 @@
-import './footer.css';
+import "./footer.css";
 import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
 import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-
-const Icones = [
-  {
-    id:1,
-    nome: 'logo-instagram',
-  },
-  {
-    id:2,
-    nome: 'logo-linkedin',
-  },
-  {
-    id:3,
-    nome: 'logo-github',
-  },
-  {
-    id:4,
-    nome: 'logo-instagram',
-  }, 
-  {
-    id:5,
-    nome: 'logo-twitter'
-  },
-]
-
-const linksNome = [
-  {
-    id:1,
-    nome: 'Home',
-  },
-  {
-    id:2,
-    nome: 'Sobre',
-  },
-  {
-    id:3,
-    nome: 'Jogo da Memoria',
-  },
-  {
-    id:4,
-    nome: 'Quiz', 
-  }, 
-  {
-    id:5,
-    nome: 'Informacoes',
-  },
-]
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Rodape() {
+
+  const [icones, setIcones] = useState([])
+  const [links, setLinks] = useState([])
+
+      useEffect(() => {
+          fetch(`https://6650c6a420f4f4c4427613db.mockapi.io/footer/icones`)
+          .then(response => response.json())
+          .then(data => {
+            setIcones(data)
+            console.log(data)
+          })
+      }, [])
+  
+  
+      useEffect(() => {
+          fetch(`https://6650c6a420f4f4c4427613db.mockapi.io/footer/links`)
+          .then(response => response.json())
+          .then(data => {
+            setLinks(data)
+            console.log(data)
+          })
+      }, [])
+
   return (
-    <footer className='footer'>
-      <ul className='FooterUlIcone'>
-        {Icones.map ( (item, index) => (
-          <li key={index}>  
-            <a className='footerAIcones' href="#">
+    <footer className="footer">
+      <ul className="FooterUlIcone">
+        {icones && icones.map((icone) => (
+          <li key={icone.id}>  
+            <a className="footerAIcones" href="#">
               <ion-icon 
-                name={item.nome}/>
+                name={icone.nome}/>
             </a>
           </li>
         ) ) }
       </ul>
 
-      <ul className='footerUlTexto FooterUlIcone'>
-        {linksNome.map ((item, index) => (
-          <li className='footerLiTexto' key={index}>
-            <a className='footerATexto footerAIcones' href="#">{item.nome}</a>
+      <ul className="footerUlTexto FooterUlIcone">
+        {links && links.map((link) => (
+          <li className="footerLiTexto" key={link.id}>
+            <Link className="footerATexto footerAIcones" to={link.link}>
+              {link.nome}
+            </Link>
           </li>
         ))}
       </ul>
 
-      
     </footer>
   );
 }
